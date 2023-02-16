12dee684fa84a170c3bc39321d077ff67a2a8de7
daZy <danilchupak@gmail.com>
2023-02-16 20:37:05 +0200

    final project

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+import requests
+
 from flask import Flask, request
 from faker import Faker
+import pandas as pd
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return "<h1>Hello, World!</h1>"
+    return "<h2>Hello, World!</h3>"
 
 
 @app.route('/requirements/')
@@ -14,21 +18,46 @@ def get_requirements():
     with open('requirements.txt', 'r') as file:
         requirements = file.read().splitlines()
 
-    return f'<h1>Requirements:</h1> {requirements}'
+    return f'<h3>Requirements:</h3> {requirements}'
 
 
 @app.route('/generate-users/')
 def generate_users():
     fake = Faker()
-    users = [fake.name() + ' ' + fake.email() for _ in range(100)]
-
-    return f'<h1>Users:</h1> {users}'
+    users = []
+    for i in range(100):
+        name = fake.name()
+        email = fake.email()
+        users.append(f'{name} - {email}')
+    return f'<h3>Users:</h3> {users}'
 
 
 @app.route('/generate-users/<int:count>/')
 def generate_users_count(count):
-    fake_count = Faker()
-    users_count = [fake_count.name() + ' ' + fake_count.email() for _ in range(count)]
+    fake = Faker()
+    users = []
+    for i in range(count):
+        name = fake.name()
+        email = fake.email()
+        users.append(f'{name} - {email}')
+    return f'<h3>Users:</h3> {users}'
 
-    return f'<h1>Users:</h1> {users_count}'
 
+@app.route('/mean/')
+def get_mean():
+    data = pd.read_csv('hw.csv')
+    df = pd.DataFrame(data)
+    df['Height (centimeters)'] = df[' "Height(Inches)"'] * 2.54
+    df['Weight (kilograms)'] = df[' "Weight(Pounds)"'] * 0.45359237
+    mean_height = df['Height (centimeters)'].mean()
+    mean_weight = df['Weight (kilograms)'].mean()
+    return f'<h3>Средний рост:</h3> {mean_height} \n <h1>Средний вес:</h1> {mean_weight}'
+
+
+@app.route('/space/')
+def get_count_spaceman():
+    response = requests.get("http://api.open-notify.org/astros.json")
+    data = response.json()
+
+    number_of_astronauts = data["number"]
+    return f'<h3>Количество космонавтов в настоящий момент: </h3> {number_of_astronauts}'
